7e8a576f6751efd2144684cc30038c27b9ea99cd
criso <criso@OLARTE23>
2023-03-14 23:45:09 -0500

    Add project files.

---
+++ Beam.Logic/Logic.cs
@@ -0,0 +1,38 @@
+﻿namespace Beam.Logic
+{
+    public class Logic
+    {
+        private readonly string _txt;
+
+        public Logic(string txt)
+        {
+            _txt = txt;
+        }
+        public bool Validator()
+        {
+         
+            if (!(_txt[0] == '%' || _txt[0] == '&' || _txt[0] == '#'))
+            {
+                throw new Exception("La viga esta mal construida!");
+            }
+            
+            for (int i = 1; i < _txt.Length - 1; i++)
+            {
+                if (!(_txt[i].Equals('=') || _txt[i].Equals('*')))                    
+                {
+                    throw new Exception("La viga esta mal construida!");
+                }
+            }
+            for (int i = 1; i < _txt.Length - 1; i++)
+            {
+                if (_txt[i].Equals('*') && _txt[i + 1].Equals('*'))
+                {
+                    throw new Exception("La viga esta mal construida!");
+                }
+                
+            }
+            return true;
+
+        }
+    }
+}
---
+++ Exercise.UI.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using Beam.Logic;
+
+try
+{
+	var logic = new Logic[]
+{
+	new Logic ("&=*===*==*"),
+	new Logic ("#=======*"),
+	new Logic ("%========*")
+
+
+};
+
+	foreach (var logics in logic)
+	{
+		Console.WriteLine(logics.Validator());
+	}
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine(ex.Message);
+}
